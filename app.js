9404b6fad398d591616f675aa93e2929dfe8ea15
Jitendra Kumar Shah <99127748+jitendrakumar13@users.noreply.github.com>
2023-01-13 02:19:24 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
-require('dotenv').config();
 const express = require('express');
 const route=require("./routes/route.js")
 const app = express();
 const connectdb=require("./db/connect.js");
 const uri ="mongodb+srv://root:root@cluster0.ozktq3t.mongodb.net/api?retryWrites=true&w=majority";
 
-const port=process.env.PORT || 5000;
-
+ 
 app.get('/', (req, res) => {
     res.send('Hello World ');
 })
@@ -24,6 +22,6 @@ const start=async() => {
 };
 
 start();
-app.listen(process.env.port,()=>{
+app.listen(process.env.PORT,()=>{
     console.log("server is runningg 5000");
 });
